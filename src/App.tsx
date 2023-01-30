7575e400f90000eb41f41c877bd26899ad93d6ce
Viết Minh Hiếu <hieuvm1@vmodev.com>
2023-01-30 14:36:46 +0700

    feat: setup styling libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import { Button } from "antd";
 
 function App() {
-  return <div className="App">HOME</div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Test tailwind</h1>
+      <Button type="primary">Test antd</Button>
+    </div>
+  );
 }
 
 export default App;
